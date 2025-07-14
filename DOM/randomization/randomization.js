cd20a462e4fb882e9c8ca7b4c86dd5256316863e
Vedika Udgir <vedikaudgir@gmail.com>
2025-07-14 18:28:32 +0530

    randomcoloured button

--- DOM/randomization/randomization.js
+++ DOM/randomization/randomization.js
@@ -1,9 +1,15 @@
 const colorButton = document.getElementById("boxButton");
 const colorBox = document.getElementById("coloredBox");
+const buttonBox = document.getElementById("boxButton");
 
 colorButton.addEventListener("click", () => {
     const randomColor = getRandomColor();
     colorBox.style.backgroundColor = randomColor;
+});
+
+colorButton.addEventListener("click", () => {
+    const randomColor = getRandomColor();
+    buttonBox.style.backgroundColor = randomColor;
 });
 
 function getRandomColor() {
